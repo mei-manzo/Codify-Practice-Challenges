f889c84362cf0ad6ac5c888e11b2586ad3b0e459
Mei Manzo <mmanzo806@gmail.com>
2021-09-14 00:22:27 -0700

    added solutions

--- Codify_Sample_Test.py
+++ Codify_Sample_Test.py
@@ -11,7 +11,7 @@
 
 # that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
 
-# For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5. *FAIL*
+# For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5. *PASS*
 
 # Given A = [1, 2, 3], the function should return 4. *PASS*
 
@@ -71,5 +71,8 @@ def solution(A):#takes an array A
                 lowest_gap = gap_nums[0]
                 return lowest_gap
 
-print(solution([1, 3, 6, 4, 1, 2])) #need a code break for all positives
-print("solution")
+print(solution([1, 3, 6, 4, 1, 2])) #5
+print(solution([1, 2, 3])) #4
+print(solution([-1, -3])) #1
+
+
