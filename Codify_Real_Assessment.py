7808bec5aec1aaa6ee3f807035c2b02daf8b6199
Mei Manzo <mmanzo806@gmail.com>
2021-09-18 14:34:08 -0700

    added stdout code

---
+++ Codify_Real_Assessment.py
@@ -0,0 +1,18 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A, D): #array A contains N integers
+    # write your code in Python 3.6
+    pass
+
+#code for stdout
+# import sys
+
+
+# def print_to_stdout(a):
+
+#     # Here a is the array holding the objects
+#     # passed as the argument of the function
+#     print(a, file = sys.stdout)
+
+# print_to_stdout("Hello World")
